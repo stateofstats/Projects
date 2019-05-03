10c4f752a954237afc0ee521644598f340d38547
derek-peterson <depeterson23@gmail.com>
2019-05-03 07:28:42 -0500

    clean nst df. split date, teams, and scores from single string

--- nst.R
+++ nst.R
@@ -20,7 +20,14 @@ games %>%
 games_split$away <- gsub(" ", "", games_split$away)
 games_split$home <- gsub(" ", "", games_split$home) 
 
-str_extract(games_split$away, "[0-9]")
-str_extract(games_split$away, "[A-z][A-z]*")
+games_split$away_team <- str_extract(games_split$away, "[A-z][A-z]*")
+games_split$away_score <- as.numeric(str_extract(games_split$away, "[0-9]"))
+games_split$home_team <- str_extract(games_split$home, "[A-z][A-z]*")
+games_split$home_score <- as.numeric(str_extract(games_split$home, "[0-9]"))
 
+games_split %>%
+  select(-c(away, home)) -> games_split
 
+cbind(nst %>% arrange(Team) %>% select(-Game), games_split) -> df_split
+
+print(df_split)
